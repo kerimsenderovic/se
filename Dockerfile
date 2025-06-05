6e716fce0bf8741dded95322878c13d6f2dbb9b8
kerimsenderovic <kerim.senderovic@stu.ibu.edu.ba>
2025-06-05 17:08:28 +0200

    nesto1

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,13 @@
-
 FROM node:18
-
 
 WORKDIR /usr/src/app
 
-
 COPY package*.json ./
-
 
 RUN npm install --production
 
-
 COPY . .
 
-
 EXPOSE 3000
-
 
 CMD ["node", "app.js"]
--- models/db.js
+++ models/db.js
@@ -4,12 +4,12 @@ let instance = null;
 class Database {
     constructor() {
         this.connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
-});
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            port: process.env.DB_PORT || 3306,
+        });
 
         this.connection.connect((err) => {
             if (err) {
