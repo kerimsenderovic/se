8f69794592da4fa0af5c7237f5ad2777dc302df6
kerimsenderovic <kerim.senderovic@stu.ibu.edu.ba>
2025-06-05 16:59:00 +0200

    nesto

--- models/db.js
+++ models/db.js
@@ -4,11 +4,12 @@ let instance = null;
 class Database {
     constructor() {
         this.connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',         
-            database: 'task_management'
-        });
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+});
 
         this.connection.connect((err) => {
             if (err) {
