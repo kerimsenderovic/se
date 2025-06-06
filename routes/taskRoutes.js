708d4bbceccd421bc94e6dfe357b40f5adf6bc44
kerimsenderovic <kerim.senderovic@stu.ibu.edu.ba>
2025-06-06 14:00:05 +0200

    final7

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,15 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const taskController = require('../controllers/taskController');
-const userController = require('../controllers/userController');
-const projectController = require('../controllers/projectController');
 
 
-router.get('/tasks', taskController.getAllTasks);
-router.get('/tasks/:id', taskController.getTaskById);
-router.post('/tasks', taskController.createTask);
-router.put('/tasks/:id', taskController.updateTask);
-router.delete('/tasks/:id', taskController.deleteTask);
+
+router.get('/', taskController.getAllTasks);          // Will become /api/tasks
+router.get('/:id', taskController.getTaskById);      // Will become /api/tasks/:id
+router.post('/', taskController.createTask);
+router.put('/:id', taskController.updateTask);
+router.delete('/:id', taskController.deleteTask);
 
 
 module.exports = router;
