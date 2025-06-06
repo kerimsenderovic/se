124de7105be36a2fe33265287312d96eb3947672
kerimsenderovic <kerim.senderovic@stu.ibu.edu.ba>
2025-06-06 14:14:45 +0200

    final10

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
